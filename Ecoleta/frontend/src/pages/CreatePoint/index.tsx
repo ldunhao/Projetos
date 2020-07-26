11e072a275d819e6774659e6a9356524f3acc116
ldunhao <ldunhao@hotmail.com>
2020-07-26 20:40:00 -0300

    integrando leaflet

--- Ecoleta/frontend/src/pages/CreatePoint/index.tsx
+++ Ecoleta/frontend/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import { FiArrowLeft } from 'react-icons/fi'
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker} from 'react-leaflet';
 
 import './styles.css'
 
@@ -34,7 +35,7 @@ const CreatePoint = () => {
             <div className="field">
               <label htmlFor="email">E-mail</label>
               <input type="email" name="email" id="email"/>
-            </div>
+            </div> 
 
             <div className="field">
               <label htmlFor="whatsapp">Whatsapp</label>
@@ -48,6 +49,14 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione um endereço no mapa</span>
           </legend>
+
+          <Map center={[-22.9139836, -43.2342717]} zoom={15}>
+              <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[-22.9139836, -43.2342717]}/>
+          </Map> 
 
           <div className="field-group">
             <div className="field">
