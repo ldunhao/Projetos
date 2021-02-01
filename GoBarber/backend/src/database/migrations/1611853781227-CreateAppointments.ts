03bbf91a993516dd9ef592493b9e3eb85378493b
ldunhao <ldunhao@hotmail.com>
2021-02-01 15:08:57 -0300

    Finalizando o cadastro de usuarios

--- GoBarber/backend/src/database/migrations/1611853781227-CreateAppointments.ts
+++ GoBarber/backend/src/database/migrations/1611853781227-CreateAppointments.ts
@@ -10,7 +10,7 @@ export class CreateAppointments1611853781227 implements MigrationInterface {
           columns: [
             {
               name: 'id',
-              type: 'varchar',
+              type: 'uuid',
               isPrimary: true,
               generationStrategy: 'uuid',
               default: 'uuid_generate_v4()'
